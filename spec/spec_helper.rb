5b103d8b679ccde2bcc5725f0239e4ad2214a713
dannguyen <dansonguyen@gmail.com>
2013-08-09 22:40:14 -0400

    tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,28 @@ require 'wrapi'
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
+#DatabaseCleaner.strategy = :truncation
+
 RSpec.configure do |config|
-  
+   # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => ":memory:"
+)
+ActiveRecord::Migration.verbose = false

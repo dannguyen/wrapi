470465e3ec5b51ab6b5953ff3734eb95afd16e24
Dan Nguyen <dansonguyen@gmail.com>
2013-08-29 14:59:18 -0400

    twitter example

--- examples/twitter_wrangler.rb
+++ examples/twitter_wrangler.rb
@@ -1,6 +1,10 @@
+require 'json'
+require 'twitter'
+
 class TwitterWrangler
   include Wrapi::Wrangler
 
+=begin
   define_rate_errors(Twitter::RateLimit, Twitter::Something) do |manager, error|
 
     manager.remove_client(client)
@@ -9,7 +13,7 @@ class TwitterWrangler
     true
   end
 
-  
+=end
 
 
   def fetch_batch_user_timeline(user_id, twitter_opts={}, &blk)
@@ -22,12 +26,14 @@ class TwitterWrangler
 
   end
 
-  def parse_credentials
-
+  def parse_credentials(loaded_creds)
+    loaded_creds['tokens'].map do |token|
+      token.merge({})
+    end
   end
 
   def load_credentials
-
+    JSON.parse open( File.expand_path('../twitter_creds.yml', __FILE__) )
   end
 
 
--- spec/unit/wrangler_calls_spec.rb
+++ spec/unit/wrangler_calls_spec.rb
@@ -6,12 +6,9 @@ end
 
 describe 'Wrapi::Wrangler' do 
 
-    context 'convenience calls' do 
-      it 'should use manager to handle batch calls'
-      it 'should use manager to handle singular calls'
-    end
-
-    
-
+  context 'convenience calls' do 
+    it 'should use manager to handle batch calls'
+    it 'should use manager to handle singular calls'
+  end
 
 end
